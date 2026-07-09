39aede4b338865ec0687d6f7e49951a3cedcc8d7
Amirhossein-Ayoubian <a.h.ayoubian@gmail.com>
2026-07-09 11:21:24 +0330

    Feature: Add structured --json export flags

--- analyzer.py
+++ analyzer.py
@@ -2,12 +2,13 @@ import argparse
 import os
 import sys
 import gzip
+import json
 
 from models import LogEntry
 from stats import LogStats
 from security import SecurityDetector
 
-def process_log_file(file_path):
+def process_log_file(file_path, output_json):
     """
     Reads the log file line by line to process data.
     """
@@ -38,6 +39,9 @@ def process_log_file(file_path):
     stats.print_report(total_lines, corrupted_lines)
     detector.print_alerts()
 
+    if output_json:
+        save_json(file_path, total_lines, corrupted_lines, stats, detector, output_json)
+
 def main():
     parser = argparse.ArgumentParser(
         description="CLI tool for analyzing and extracting insights from web server access logs."
@@ -48,6 +52,11 @@ def main():
         type=str, 
         help="Path to the access log file that needs to be processed."
     )
+
+    parser.add_argument(
+        '--json', type=str, metavar='OUTPUT_FILE.json',
+        help="Path to save the analyzed report in structured JSON format."
+    )
     
     args = parser.parse_args()
     
@@ -56,7 +65,22 @@ def main():
         sys.exit(1)
         
     print(f"Success: File found! Preparing to process: {args.log_file}")
-    process_log_file(args.log_file)
+    process_log_file(args.log_file, args.json)
+
+def save_json(file_path, total_lines, corrupted_lines, stats, detector, output_json):
+    combined_report = {
+        "metadata": {
+            "file_processed": file_path,
+            "total_lines": total_lines,
+            "corrupted_lines_skipped": corrupted_lines,
+        },
+        "analytics": stats.to_dict(),
+        "security_anomalies": detector.to_dict()
+    }
+    
+    with open(output_json, 'w', encoding='utf-8') as json_file:
+        json.dump(combined_report, json_file, indent=4, ensure_ascii=False)
+    print(f"Success: Structured JSON report saved to '{output_json}'")
 
 if __name__ == "__main__":
     main()
